78107455a0b7c0a9df22501865aeb8a12f0bcabf
manami-project <13179375+manami-project@users.noreply.github.com>
2020-08-06 19:52:09 +0200

    Updated modb-core to 2.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ version = project.findProperty("releaseVersion") as String? ?: ""
 val projectName = "modb-kitsu"
 
 dependencies {
-    api("io.github.manamiproject:modb-core:2.0.4")
+    api("io.github.manamiproject:modb-core:2.0.5")
     api(kotlin("stdlib-jdk8"))
 
     implementation("org.jsoup:jsoup:1.13.1")
