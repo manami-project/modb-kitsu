5b90ef5c656124bb4b9e043f1059f20d02820d0a
manami-project <13179375+manami-project@users.noreply.github.com>
2020-09-24 19:02:00 +0200

    Updated modb-test to 1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
     implementation("org.jsoup:jsoup:1.13.1")
 
     testImplementation("ch.qos.logback:logback-classic:1.2.3")
-    testImplementation("io.github.manamiproject:modb-test:1.0.4")
+    testImplementation("io.github.manamiproject:modb-test:1.1.0")
 }
 
 kotlin {
