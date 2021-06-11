fe8d893a7d5e5e24577238e32f0a9ad30a094ae6
manami-project <13179375+manami-project@users.noreply.github.com>
2021-06-11 18:30:35 +0200

    Updated modb-core to 4.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ group = "io.github.manamiproject"
 version = project.findProperty("release.version") as String? ?: ""
 
 dependencies {
-    api("io.github.manamiproject:modb-core:4.0.0")
+    api("io.github.manamiproject:modb-core:4.0.1")
     api(kotlin("stdlib-jdk8"))
 
     implementation(platform(kotlin("bom")))
