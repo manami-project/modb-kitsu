131dd7b4bf50f70c06701b0db9d2327ee6e2c1c9
manami-project <13179375+manami-project@users.noreply.github.com>
2023-11-01 19:26:05 +0100

    Use JavaVersion enum in gradle script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-val jvmTarget = "21"
-
 plugins {
     kotlin("jvm") version "1.9.20"
     `maven-publish`
@@ -46,12 +44,12 @@ dependencies {
 
 kotlin {
     explicitApi()
-    jvmToolchain(jvmTarget.toInt())
+    jvmToolchain(JavaVersion.VERSION_21.toString().toInt())
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions {
-        jvmTarget = jvmTarget
+        jvmTarget = JavaVersion.VERSION_21.toString()
     }
 }
 
