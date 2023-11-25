92eafe5c51e261fa441f304333385d5910d9b1ab
manami-project <13179375+manami-project@users.noreply.github.com>
2023-11-25 16:11:46 +0100

    Added test results in PRs

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,7 +56,7 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
 tasks.withType<Test> {
     useJUnitPlatform()
     reports.html.required.set(false)
-    reports.junitXml.required.set(false)
+    reports.junitXml.required.set(true)
     maxParallelForks = Runtime.getRuntime().availableProcessors()
 }
 
